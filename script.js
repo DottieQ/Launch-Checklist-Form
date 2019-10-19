2c2ee2c05b65dab59665c0013a6228cc74630e02
Dottie <dottiequick@gmail.com>
2019-10-19 14:43:11 -0500

    got validation and shuttle requirements finished, need to fix JSON

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 window.addEventListener("load", function() {
-//    fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
-//       response.json().then( function(json) {
-      const missionDestination = document.getElementById("missionTarget");
-      missionDestination.innerHTML = "<h2>Mission Destination</h2>"
-//`
-//          <h2>Mission Destination</h2>
-//             <ol>
-//                <li>Name: ${json.name[0]}</li>
-//                <li>Diameter: ${json.diameter[0]}</li>
-//                <li>Star: ${json.star[0]}</li>
-//                <li>Distance from Earth: ${json.distance[0]}</li>
-//                <li>Number of Moons: ${json.moons[0]}</li>
-//             </ol>
-//             <img src="${json.image[0]}">
-//             `;
-//       });
+   const missionDestination = document.getElementById("missionTarget");
+   missionDestination.innerHTML = 
+   '<div>   <h2>Mission Destination</h2><ol><li>hi</li> </ol>  </div>'
 
-// });
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then( function(json) {
+`
+            <ol>
+               <li>Name: ${json.name[0]}</li>
+               <li>Diameter: ${json.diameter[0]}</li>
+               <li>Star: ${json.star[0]}</li>
+               <li>Distance from Earth: ${json.distance[0]}</li>
+               <li>Number of Moons: ${json.moons[0]}</li>
+            </ol>
+            <img src="${json.image[0]}">
+            `;
+      });
+
+});
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
       let pilotNameInput = document.querySelector("input[name=pilotName]");
@@ -29,18 +30,13 @@ window.addEventListener("load", function() {
       if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoWeightInput.value === "") {
          alert("All fields are required!");
          event.preventDefault();
-      }
-
-      if (isNaN(fuelLevelInput) || isNaN(cargoWeightInput)) {         
+      } else if (isNaN(fuelLevelInput) || isNaN(cargoWeightInput)) {         
          alert("Please enter numbers in the Fuel Level and Cargo Mass fields.");
          event.preventDefault();
-      }
-   
-      if (isNaN(pilotNameInput.value) && isNaN(copilotNameInput.value)) {
+      } else if (isNaN(pilotNameInput.value) && isNaN(copilotNameInput.value)) {
+         let faultyItems = document.getElementById("faultyItems");
+         faultyItems.style.visibility = "visible";
          if (fuelLevelInput < 10000 || cargoWeightInput > 10000) {
-            let faultyItems = document.getElementById("faultyItems");
-            faultyItems.style.visibility = "visible";
-            
             let pilotStatusUpdate = document.getElementById("pilotStatus");
             let coPilotStatusUpdate = document.getElementById("copilotStatus");
             let launchStatusUpdate = document.getElementById("launchStatus");
@@ -58,15 +54,20 @@ window.addEventListener("load", function() {
 
             if (fuelLevelInput < 10000) {
             checkList.innerHTML += `<li>Fuel level too low for launch.</li>`;
+            event.preventDefault();
             }
 
             if (cargoWeightInput > 10000) {
                checkList.innerHTML += `<li>Cargo mass too high for launch.</li>`;
+               event.preventDefault();
             }
-            alert("!"); 
          } else {
+            let faultyItems = document.getElementById("faultyItems");
+            faultyItems.style.visibility = "visible";
+            let launchStatusUpdate = document.getElementById("launchStatus");
             launchStatusUpdate.innerHTML = "Shuttle is ready for launch"
-            launchStatusUpdate.style.color = "green";  
+            launchStatusUpdate.style.color = "green";
+            event.preventDefault();
          }
  
       } else {
@@ -75,6 +76,8 @@ window.addEventListener("load", function() {
       } 
 
    });
+
+
 
 
 
